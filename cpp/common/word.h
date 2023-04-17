2e17c0bd0e097e215f37ef4ace934d3e6fac51bd
Alex Hirsch <hirsch@fantom.foundation>
2023-04-17 14:07:49 +0200

    C++: Add support for basic operators to Word

--- cpp/common/word.h
+++ cpp/common/word.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <compare>
 #include <cstddef>
 #include <cstdint>
 #include <initializer_list>
@@ -8,14 +9,17 @@
 
 namespace tosca {
 
-// A Word is a single 32-byte fixed-size byte vector without numerical interpretation. Words are trivial objects of
-// fixed size that can be serialized by copying their byte pattern.
+// A Word is a single 32-byte fixed-size byte vector representing an unsigned
+// 256 bit integer. Words are trivial objects of fixed size that can be
+// serialized by copying their byte pattern.
 //
-// By default, new instances are not initialized. Thus, when defining a local variable
+// By default, new instances are not initialized. Thus, when defining a local
+// variable
 //
 //   Word word;  // the value is undefined
 //
-// the value is not defined. If zero-initialization is desired, an empty initializer list can be added:
+// the value is not defined. If zero-initialization is desired, an empty
+// initializer list can be added:
 //
 //   Word word{};  // the value is zero
 //
@@ -24,19 +28,48 @@ class Word {
  public:
   Word() = default;
 
-  // A convenience constructor supporting integer literals for initializing word. Elements not listed in the initializer
-  // will be initialized with zero. Elements exceeding the 32 entry limit will be ignored.
+  // A convenience constructor supporting integer literals for initializing
+  // word. Elements not listed in the initializer will be initialized with zero.
+  // The given sequence ends with the least significant byte. Elements beyond
+  // the most significant byte (32) are ignored.
   Word(std::initializer_list<const std::uint8_t> list);
 
-  // Supports equality comparison. Note: since words do not exhibit a numerical interpretation, the less-than order
-  // would not be well defined.
-  bool operator==(const Word&) const = default;
+  // Byte offset starting from the most significant byte. Returns 0 on
+  // out-of-bounds access.
+  std::byte operator[](std::uint8_t) const;
+  std::byte operator[](const Word&) const;
 
-  // Prints a word as a hax string in upper case.
+  bool operator==(const Word&) const = default;
+  std::strong_ordering operator<=>(const Word& other) const;
+
+  Word operator+(const Word&) const;
+  Word operator-(const Word&) const;
+  Word operator*(const Word&) const;
+
+  // If the denominator is 0, the result will be 0.
+  Word operator/(const Word&) const;
+  Word operator%(const Word&) const;
+  Word SignedDiv(const Word&) const;
+  Word SignedMod(const Word&) const;
+
+  Word operator<<(const Word&) const;
+  Word operator>>(const Word&) const;
+  Word operator|(const Word&) const;
+  Word operator&(const Word&) const;
+  Word operator^(const Word&) const;
+  Word operator~() const;
+
+  Word Exp(const Word&) const;
+
+  // Prints a word as a hex string in upper case.
   friend std::ostream& operator<<(std::ostream&, const Word&);
+
+  static const Word kMax;
 
  private:
   std::array<std::byte, 32> data_;
 };
+
+inline const Word Word::kMax = ~Word{0};
 
 }  // namespace tosca
---
+++ cpp/third_party/intx/intx.BUILD
@@ -0,0 +1,6 @@
+cc_library(
+    name = "intx",
+    hdrs = ["include/intx/intx.hpp"],
+    includes = ["include"],
+    visibility = ["//visibility:public"],
+)
