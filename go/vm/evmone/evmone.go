4628b41c16348ffd17b2e39a55b5b28ca58fec70
Herbert Jordan <herbert@swrnd.ch>
2023-04-26 16:00:43 +0200

    Fix relative path library issue

--- go/vm/evmone/evmone.go
+++ go/vm/evmone/evmone.go
@@ -1,13 +1,17 @@
 package evmone
 
+/*
+#cgo LDFLAGS: -Wl,-rpath,${SRCDIR}/../../../third_party/evmone/build/lib
+*/
+import "C"
+
 import (
 	"github.com/Fantom-foundation/Tosca/go/common"
 	"github.com/ethereum/go-ethereum/core/vm"
 )
 
 func NewInterpreter(evm *vm.EVM, cfg vm.Config) vm.EVMInterpreter {
-	path := "../../../third_party/evmone/build/lib/libevmone.so"
-	return common.NewEVMCInterpreter(path, evm, cfg)
+	return common.NewEVMCInterpreter("libevmone.so", evm, cfg)
 }
 
 func init() {
