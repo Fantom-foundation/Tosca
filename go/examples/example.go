263f97325b8ebf55e1eeafa41877179e22b1ac35
Herbert Jordan <herbert@swrnd.ch>
2023-05-01 16:11:21 +0200

    Factor out and generalize VM testing based on fib example

---
+++ go/examples/example.go
@@ -0,0 +1,79 @@
+package examples
+
+import (
+	"fmt"
+	"math"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/vm"
+)
+
+// Example describes a contract and an entry point with a (int)->int signature.
+type Example struct {
+	code      []byte        // some contract code
+	function  uint32        // identifier of the function in the contract to be called
+	reference func(int) int // a reference function computing the same function
+}
+
+type Result struct {
+	Result  int
+	UsedGas int64
+}
+
+// RunOn runs this example on the given interpreter, using the given argument.
+func (e *Example) RunOn(interpreter vm.EVMInterpreter, argument int) (Result, error) {
+	const initialGas = math.MaxInt64
+	input := encodeArgument(e.function, argument)
+
+	// create a dummy contract
+	addr := vm.AccountRef{}
+	contract := vm.NewContract(addr, addr, big.NewInt(0), initialGas)
+	contract.Code = e.code
+	contract.CodeAddr = &common.Address{}
+
+	output, err := interpreter.Run(contract, input, false)
+	if err != nil {
+		return Result{}, err
+	}
+
+	result, err := decodeOutput(output)
+	if err != nil {
+		return Result{}, err
+	}
+	return Result{
+		Result:  result,
+		UsedGas: initialGas - int64(contract.Gas),
+	}, nil
+}
+
+// RunRef runs the reference function of this example to produce the expected result.
+func (e *Example) RunReference(argument int) int {
+	return e.reference(argument)
+}
+
+func encodeArgument(function uint32, arg int) []byte {
+	// see details of argument encoding: t.ly/kBl6
+	data := make([]byte, 4+32) // parameter is padded up to 32 bytes
+
+	// encode function selector in big-endian format
+	data[0] = byte(function >> 24)
+	data[1] = byte(function >> 16)
+	data[2] = byte(function >> 8)
+	data[3] = byte(function)
+
+	// encode argument as a big-endian value
+	data[4+28] = byte(arg >> 24)
+	data[5+28] = byte(arg >> 16)
+	data[6+28] = byte(arg >> 8)
+	data[7+28] = byte(arg)
+
+	return data
+}
+
+func decodeOutput(output []byte) (int, error) {
+	if len(output) != 32 {
+		return 0, fmt.Errorf("unexpected length of end; wanted 32, got %d", len(output))
+	}
+	return (int(output[28]) << 24) | (int(output[29]) << 16) | (int(output[30]) << 8) | (int(output[31]) << 0), nil
+}
---
+++ go/vm/vm_test.go
@@ -0,0 +1,110 @@
+package vm
+
+import (
+	"fmt"
+	"math/big"
+	"testing"
+
+	"github.com/Fantom-foundation/Tosca/go/examples"
+	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/ethereum/go-ethereum/params"
+
+	_ "github.com/Fantom-foundation/Tosca/go/vm/evmone"
+	_ "github.com/Fantom-foundation/Tosca/go/vm/lfvm"
+)
+
+var (
+	variants = []string{"geth", "lfvm", "lfvm-si", "evmone"}
+)
+
+func TestFib_ComputesCorrectResult(t *testing.T) {
+	evm := newTestEVM(London)
+	fib := examples.GetFibExample()
+	for _, variant := range variants {
+		interpreter := vm.NewInterpreter(variant, evm, vm.Config{})
+		for i := 0; i < 10; i++ {
+			t.Run(fmt.Sprintf("%s-%d", variant, i), func(t *testing.T) {
+				want := fib.RunReference(i)
+				got, err := fib.RunOn(interpreter, i)
+				if err != nil {
+					t.Fatalf("error processing contract: %v", err)
+				}
+				if want != got.Result {
+					t.Fatalf("incorrect result, wanted %d, got %d", want, got.Result)
+				}
+			})
+		}
+	}
+}
+
+func TestFib_ComputesCorrectGasPrice(t *testing.T) {
+	fib := examples.GetFibExample()
+	for _, revision := range revisions {
+		evm := newTestEVM(revision)
+		reference := vm.NewInterpreter("geth", evm, vm.Config{})
+		for _, variant := range variants {
+			interpreter := vm.NewInterpreter(variant, evm, vm.Config{})
+			for i := 0; i < 10; i++ {
+				t.Run(fmt.Sprintf("%s-%s-%d", revision, variant, i), func(t *testing.T) {
+					want, err := fib.RunOn(reference, i)
+					if err != nil {
+						t.Fatalf("failed to run reference VM: %v", err)
+					}
+
+					got, err := fib.RunOn(interpreter, i)
+					if err != nil {
+						t.Fatalf("error processing contract: %v", err)
+					}
+
+					if want.UsedGas != got.UsedGas {
+						t.Errorf("incorrect gas usage, wanted %d, got %d", want.UsedGas, got.UsedGas)
+					}
+				})
+			}
+		}
+	}
+}
+
+type Revision int
+
+const (
+	Istanbul Revision = 1
+	Berlin   Revision = 2
+	London   Revision = 3
+)
+
+var revisions = []Revision{Istanbul, Berlin, London}
+
+func (r Revision) String() string {
+	switch r {
+	case Istanbul:
+		return "Istanbul"
+	case Berlin:
+		return "Berlin"
+	case London:
+		return "London"
+	}
+	return "Unknown"
+}
+
+func newTestEVM(r Revision) *vm.EVM {
+	// Configure the block numbers for revision changes.
+	chainConfig := params.AllEthashProtocolChanges
+	chainConfig.BerlinBlock = big.NewInt(10)
+	chainConfig.LondonBlock = big.NewInt(20)
+
+	// Chose the block height to run.
+	block := 5
+	if r == Berlin {
+		block = 15
+	} else if r == London {
+		block = 25
+	}
+
+	blockCtxt := vm.BlockContext{
+		BlockNumber: big.NewInt(int64(block)),
+	}
+	txCtxt := vm.TxContext{}
+	config := vm.Config{}
+	return vm.NewEVM(blockCtxt, txCtxt, nil, chainConfig, config)
+}
