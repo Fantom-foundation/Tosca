4bdaba3b5da23cba471f3bb661a2ba5947694ef6
Herbert Jordan <herbert@swrnd.ch>
2023-05-02 18:27:21 +0200

    Add support for recursive contract calls

--- go/vm/evmone/evmone.go
+++ go/vm/evmone/evmone.go
@@ -11,6 +11,10 @@ import (
 )
 
 func NewInterpreter(evm *vm.EVM, cfg vm.Config) vm.EVMInterpreter {
+	// In the CGO instructions at the top of this file the build directory
+	// of the evmone project is added to the rpath of the resulting library.
+	// This way, the libevmone.so file can be found during runtime, even if
+	// the LD_LIBRARY_PATH is not set accordingly.
 	return common.NewEVMCInterpreter("libevmone.so", evm, cfg)
 }
 
