82536bb530b65faaa50817c66cbe7d5b404b067b
Herbert Jordan <herbert@swrnd.ch>
2023-05-04 10:55:15 +0200

    Add support for evmone basic and advanced mode

--- go/vm/evmone/evmone_test.go
+++ go/vm/evmone/evmone_test.go
@@ -7,6 +7,12 @@ import (
 	"github.com/ethereum/go-ethereum/core/vm"
 )
 
+var variants = []string{
+	"evmone",
+	"evmone-basic",
+	"evmone-advanced",
+}
+
 func TestFib10(t *testing.T) {
 	const arg = 10
 
@@ -15,21 +21,29 @@ func TestFib10(t *testing.T) {
 	// compute expected value
 	wanted := example.RunReference(arg)
 
-	interpreter := NewInterpreter(&vm.EVM{}, vm.Config{})
+	for _, variant := range variants {
+		t.Run(variant, func(t *testing.T) {
+			interpreter := vm.NewInterpreter(variant, &vm.EVM{}, vm.Config{})
 
-	got, err := example.RunOn(interpreter, arg)
-	if err != nil {
-		t.Fatalf("running the fib example failed: %v", err)
-	}
+			got, err := example.RunOn(interpreter, arg)
+			if err != nil {
+				t.Fatalf("running the fib example failed: %v", err)
+			}
 
-	if got.Result != wanted {
-		t.Fatalf("unexpected result, wanted %v, got %v", wanted, got.Result)
+			if got.Result != wanted {
+				t.Fatalf("unexpected result, wanted %v, got %v", wanted, got.Result)
+			}
+		})
 	}
 }
 
 func BenchmarkNewEvmcInterpreter(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		NewInterpreter(&vm.EVM{}, vm.Config{})
+	for _, variant := range variants {
+		b.Run(variant, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				vm.NewInterpreter(variant, &vm.EVM{}, vm.Config{})
+			}
+		})
 	}
 }
 
@@ -43,16 +57,20 @@ func benchmarkFib(b *testing.B, arg int) {
 	// compute expected value
 	wanted := example.RunReference(arg)
 
-	interpreter := NewInterpreter(&vm.EVM{}, vm.Config{})
+	for _, variant := range variants {
+		b.Run(variant, func(b *testing.B) {
+			interpreter := vm.NewInterpreter(variant, &vm.EVM{}, vm.Config{})
 
-	for i := 0; i < b.N; i++ {
-		got, err := example.RunOn(interpreter, arg)
-		if err != nil {
-			b.Fatalf("running the fib example failed: %v", err)
-		}
+			for i := 0; i < b.N; i++ {
+				got, err := example.RunOn(interpreter, arg)
+				if err != nil {
+					b.Fatalf("running the fib example failed: %v", err)
+				}
 
-		if wanted != got.Result {
-			b.Fatalf("unexpected result, wanted %d, got %d", wanted, got)
-		}
+				if wanted != got.Result {
+					b.Fatalf("unexpected result, wanted %d, got %d", wanted, got)
+				}
+			}
+		})
 	}
 }
