f50090a90c16b207dce80283f4f1f6bb658dfefa
Alex Hirsch <hirsch@fantom.foundation>
2023-06-07 14:50:12 +0200

    evmzero: Add evmc interface

--- cpp/vm/evmzero/evmzero.cc
+++ cpp/vm/evmzero/evmzero.cc
@@ -1,10 +1,88 @@
 #include <evmc/evmc.h>
 #include <evmc/utils.h>
 
+#include "vm/evmzero/interpreter.h"
+
 namespace tosca::evmzero {
 
+evmc_status_code ToEvmcStatusCode(RunState state) {
+  switch (state) {
+    case RunState::kRunning:
+      return EVMC_FAILURE;
+    case RunState::kDone:
+      return EVMC_SUCCESS;
+    case RunState::kRevert:
+      return EVMC_REVERT;
+    case RunState::kInvalid:
+      return EVMC_INVALID_INSTRUCTION;
+    case RunState::kErrorOpcode:
+      return EVMC_INVALID_INSTRUCTION;
+    case RunState::kErrorGas:
+      return EVMC_OUT_OF_GAS;
+    case RunState::kErrorStack:
+      return EVMC_STACK_UNDERFLOW;
+    case RunState::kErrorJump:
+      return EVMC_BAD_JUMP_DESTINATION;
+    case RunState::kErrorCall:
+      return EVMC_CALL_DEPTH_EXCEEDED;
+    case RunState::kErrorCreate:
+      return EVMC_FAILURE;
+  }
+  return EVMC_FAILURE;
+}
+
+// This class represents the evmzero virtual machine (VM) which consists
+// primarily of the evmzero interpreter. This class connects evmzero to the host
+// infrastructure via the evmc interface.
+class VM : public evmc_vm {
+ public:
+  constexpr VM() noexcept
+      : evmc_vm{
+            .abi_version = EVMC_ABI_VERSION,
+            .name = "evmzero",
+            .version = "0.1.0",
+
+            .destroy = [](evmc_vm* vm) { delete static_cast<VM*>(vm); },
+
+            .execute = [](evmc_vm* vm, const evmc_host_interface* host_interface, evmc_host_context* host_context,
+                          evmc_revision revision, const evmc_message* message, const uint8_t* code,
+                          size_t code_size) -> evmc_result {
+              return static_cast<VM*>(vm)->execute({code, code_size}, message, host_interface, host_context, revision);
+            },
+
+            .get_capabilities = [](evmc_vm*) -> evmc_capabilities_flagset { return EVMC_CAPABILITY_EVM1; },
+        } {}
+
+  evmc_result execute(std::span<const uint8_t> code, const evmc_message* message,                  //
+                      const evmc_host_interface* host_interface, evmc_host_context* host_context,  //
+                      evmc_revision) {
+    auto interpreter_result = Interpret({
+        .code = code,
+        .message = message,
+        .host_interface = host_interface,
+        .host_context = host_context,
+    });
+
+    // Move output data to a dedicated buffer so we can release the interpreter
+    // result.
+    uint8_t* output_data = nullptr;
+    if (!interpreter_result.return_data.empty()) {
+      output_data = new uint8_t[interpreter_result.return_data.size()];
+      std::copy(interpreter_result.return_data.begin(), interpreter_result.return_data.end(), output_data);
+    }
+
+    return {
+        .status_code = ToEvmcStatusCode(interpreter_result.state),
+        .gas_left = static_cast<int64_t>(interpreter_result.remaining_gas),
+        .output_data = output_data,
+        .output_size = interpreter_result.return_data.size(),
+        .release = [](const evmc_result* result) { delete[] result->output_data; },
+    };
+  }
+};
+
 extern "C" {
-EVMC_EXPORT evmc_vm* evmc_create_evmzero() noexcept { return nullptr; }
+EVMC_EXPORT evmc_vm* evmc_create_evmzero() noexcept { return new VM; }
 }
 
 }  // namespace tosca::evmzero
