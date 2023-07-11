361956fcef267f280781a754b82b43817fdb0836
HonzaDajc <jan.dajc@gmail.com>
2023-07-11 10:24:17 +0200

    Fix typo

--- go/vm/test/dynamic_gas.go
+++ go/vm/test/dynamic_gas.go
@@ -686,7 +686,7 @@ func gasDynamicSelfDestruct(revision Revision) []*DynGasTest {
 	}
 
 	for i, test := range tests {
-		// Offset target adres from contract addres
+		// Offset target address from contract addres
 		targetAddress := common.Address{byte(i + 1)}
 		contractAddress := common.Address{0}
 		empty := test.targetAddrEmpty
