f819021aa77a4c5801a66cad936735e7aff8b346
HonzaDajc <jan.dajc@gmail.com>
2023-07-12 15:19:20 +0200

    Common way of setting chainconfig (#106)
    * Use common chain config setup

--- go/vm/test/test_utils.go
+++ go/vm/test/test_utils.go
@@ -37,15 +37,11 @@ type TestEVM struct {
 
 func GetCleanEVM(revision Revision, interpreter string, stateDB vm.StateDB) TestEVM {
 	// Set hard forks for chainconfig
-	chainConfig := params.ChainConfig{
-		ChainID:       big.NewInt(0),
-		EIP150Block:   big.NewInt(0),
-		EIP155Block:   big.NewInt(0),
-		EIP158Block:   big.NewInt(0),
-		IstanbulBlock: big.NewInt(Istanbul.GetForkBlock()),
-		BerlinBlock:   big.NewInt(Berlin.GetForkBlock()),
-		LondonBlock:   big.NewInt(London.GetForkBlock()),
-	}
+	chainConfig := params.AllEthashProtocolChanges
+	chainConfig.ChainID = big.NewInt(0)
+	chainConfig.IstanbulBlock = big.NewInt(Istanbul.GetForkBlock())
+	chainConfig.BerlinBlock = big.NewInt(Berlin.GetForkBlock())
+	chainConfig.LondonBlock = big.NewInt(London.GetForkBlock())
 
 	// Hashing function used in the context for BLOCKHASH instruction
 	getHash := func(num uint64) common.Hash {
@@ -71,7 +67,7 @@ func GetCleanEVM(revision Revision, interpreter string, stateDB vm.StateDB) Test
 	config := vm.Config{
 		InterpreterImpl: interpreter,
 	}
-	return TestEVM{vm.NewEVM(blockCtx, txCtx, stateDB, &chainConfig, config)}
+	return TestEVM{vm.NewEVM(blockCtx, txCtx, stateDB, chainConfig, config)}
 }
 
 type RunResult struct {
