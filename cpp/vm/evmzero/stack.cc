bc16fd7ae450b47d7b28fecfcc8e67dc23761b10
Herbert Jordan <herbert@swrnd.ch>
2023-07-31 14:41:11 +0200

    Add re-use list for stack data instances

--- cpp/vm/evmzero/stack.cc
+++ cpp/vm/evmzero/stack.cc
@@ -4,26 +4,32 @@
 
 namespace tosca::evmzero {
 
-Stack::Stack() : stack_(std::make_unique<Data>()), top_(stack_->end()), end_(top_) {}
+Stack::Stack() : data_(Data::Get()), top_(data_->end()), end_(top_) {}
 
 Stack::Stack(std::initializer_list<uint256_t> elements) : Stack() {
-  TOSCA_ASSERT(elements.size() <= stack_->size());
+  TOSCA_ASSERT(elements.size() <= data_->size());
   for (auto cur : elements) {
     Push(cur);
   }
 }
 
 Stack::Stack(const Stack& other) : Stack() {
-  *stack_ = *other.stack_;
-  top_ = stack_->end() - other.GetSize();
+  *data_ = *other.data_;
+  top_ = data_->end() - other.GetSize();
+}
+
+Stack::~Stack() {
+  if (data_) {
+    data_->Release();
+  }
 }
 
 Stack& Stack::operator=(const Stack& other) {
   if (this == &other) {
     return *this;
   }
-  *stack_ = *other.stack_;
-  top_ = stack_->end() - other.GetSize();
+  *data_ = *other.data_;
+  top_ = data_->end() - other.GetSize();
   return *this;
 }
 
@@ -40,6 +46,32 @@ std::ostream& operator<<(std::ostream& out, const Stack& stack) {
     out << stack[i];
   }
   return out << " ]B";
+}
+
+// kFreeList is the head of a lock-free synchronized linked list of Data instances that
+// are free to be reused by arbitrary threads. The list size is limited to the maximum
+// number simultaneously used stack instances and once allocated stacks are never released.
+std::atomic<Stack::Data*> Stack::Data::freeList = nullptr;
+
+Stack::Data* Stack::Data::Get() {
+  Data* res = freeList.load(std::memory_order_relaxed);
+  for (;;) {
+    if (res == nullptr) {
+      return new Data();
+    }
+    // Try to retrieve the instance by updating the head pointer. If successful,
+    // the instance can be used by this thread. If not, res is updated to the new
+    // head of the list.
+    if (freeList.compare_exchange_weak(res, res->next_)) {
+      return res;
+    }
+  }
+}
+
+void Stack::Data::Release() {
+  // On the first call next_ is updated, and on subsequent calls the head is replaced.
+  while (!freeList.compare_exchange_weak(next_, this, std::memory_order_relaxed, std::memory_order_relaxed)) {
+  }
 }
 
 }  // namespace tosca::evmzero
