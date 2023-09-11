ca24cb686379d524daeb794cca32addc1c944d80
BlackMark <me@blackmark.me>
2023-09-11 15:22:57 -0500

    evmzero: Add information about build settings to profiler output through cmake

--- cpp/common/macros.h
+++ cpp/common/macros.h
@@ -18,14 +18,3 @@
 
 #define TOSCA_STRINGIFY_(str) #str
 #define TOSCA_STRINGIFY(str) TOSCA_STRINGIFY_(str)
-
-#ifdef __clang__
-#define TOSCA_COMPILER "clang " __clang_version__
-#elif __GNUC__
-#define TOSCA_COMPILER \
-  "gcc " TOSCA_STRINGIFY(__GNUC__) "." TOSCA_STRINGIFY(__GNUC_MINOR__) "." TOSCA_STRINGIFY(__GNUC_PATCHLEVEL__)
-#elif _MSC_VER
-#define TOSCA_COMPILER "msvc " TOSCA_STRINGIFY(_MSC_VER)
-#else
-#define TOSCA_COMPILER "unknown"
-#endif
