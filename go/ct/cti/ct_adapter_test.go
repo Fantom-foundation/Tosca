08b12fe946c3c600cf023c9e4a1ec682d5f2392b
Alex Hirsch <hirsch@fantom.foundation>
2023-10-02 11:53:07 +0200

    cti: Add ct adapter

---
+++ go/ct/cti/ct_adapter_test.go
@@ -0,0 +1,30 @@
+package cti
+
+import (
+	"testing"
+
+	"github.com/Fantom-foundation/Tosca/go/ct"
+	"github.com/holiman/uint256"
+)
+
+func TestCtiAdapter(t *testing.T) {
+	s := ct.State{
+		Status: ct.Running,
+		Gas:    100,
+		Code:   []byte{ct.ADD},
+		Stack:  ct.NewStack([]uint256.Int{*uint256.NewInt(21), *uint256.NewInt(42)}),
+	}
+
+	var evm CtAdapter
+
+	s, err := evm.StepN(s, 2)
+	if err != nil {
+		t.Error(err)
+	}
+
+	top := s.Stack.Get(0)
+
+	if s.Status != ct.Stopped || !top.Eq(uint256.NewInt(21+42)) {
+		t.Fail()
+	}
+}
