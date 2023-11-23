003910fc0d2e0372b52b6196106e9a54c5da1983
Alex Hirsch <hirsch@fantom.foundation>
2023-11-23 17:01:12 +0100

    ct: Remove rule coverage unit test

--- go/ct/spc/specification_test.go
+++ go/ct/spc/specification_test.go
@@ -6,39 +6,7 @@ import (
 	"pgregory.net/rand"
 
 	"github.com/Fantom-foundation/Tosca/go/ct/gen"
-	"github.com/Fantom-foundation/Tosca/go/ct/st"
 )
-
-func TestSpecification_RulesCoverTestCases(t *testing.T) {
-	rules := Spec.GetRules()
-	for _, rule := range rules {
-		rule := rule
-		t.Run(rule.Name, func(t *testing.T) {
-			t.Parallel()
-
-			// TODO: For now, check that we get at least one rule matching for
-			// the full set of test cases (and at most one rule for every test
-			// case). Later we'll enforce that exactly one rule applies to every
-			// single test case.
-			atLeastOne := false
-
-			rule.EnumerateTestCases(rand.New(0), func(state *st.State) error {
-				rules := Spec.GetRulesFor(state)
-				if len(rules) > 0 {
-					atLeastOne = true
-				}
-				if len(rules) > 1 {
-					t.Fatalf("multiple rules for state %v: %v", state, rules)
-				}
-				return nil
-			})
-
-			if !atLeastOne {
-				t.Fatalf("No rule matches any of the generated test cases")
-			}
-		})
-	}
-}
 
 func TestSpecification_RulesCoverRandomStates(t *testing.T) {
 	const N = 10000
