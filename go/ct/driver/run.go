84be31a216f59d6983e420f575777cabe1d76f73
Herbert Jordan <herbert@swrnd.ch>
2024-03-19 13:56:08 +0100

    Improve load balancing of CT test runner (#330)
    * Run all generated test cases on the interpreter under test (#333)
    
    * Improve CT test runner
    
    * Perform test state generation in parallel
    
    * Fix termination on number of issues
    
    * Update go/ct/driver/run.go
    
    Co-authored-by: BlackMark <me@blackmark.me>
    
    * Fix new package dependencies
    
    ---------
    
    Co-authored-by: BlackMark <me@blackmark.me>

--- go/ct/driver/run.go
+++ go/ct/driver/run.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"math"
 	"os"
 	"regexp"
 	"runtime"
@@ -11,15 +11,16 @@ import (
 	"sync/atomic"
 	"time"
 
-	"pgregory.net/rand"
-
 	"github.com/Fantom-foundation/Tosca/go/ct"
 	"github.com/Fantom-foundation/Tosca/go/ct/rlz"
 	"github.com/Fantom-foundation/Tosca/go/ct/spc"
 	"github.com/Fantom-foundation/Tosca/go/ct/st"
 	"github.com/Fantom-foundation/Tosca/go/vm/evmzero"
 	"github.com/Fantom-foundation/Tosca/go/vm/lfvm"
+	"github.com/dsnet/golib/unitconv"
 	"github.com/urfave/cli/v2"
+	"golang.org/x/exp/maps"
+	"pgregory.net/rand"
 )
 
 var RunCmd = cli.Command{
@@ -40,8 +41,8 @@ var RunCmd = cli.Command{
 		},
 		&cli.IntFlag{
 			Name:  "max-errors",
-			Usage: "maximum number of errors to display (0 displays all errors)",
-			Value: 1,
+			Usage: "aborts testing after the given number of issues",
+			Value: -1,
 		},
 		&cli.Uint64Flag{
 			Name:  "seed",
@@ -83,11 +84,7 @@ func doRun(context *cli.Context) error {
 
 	evm, ok := evms[evmIdentifier]
 	if !ok {
-		availableIdentifiers := make([]string, 0, len(evms))
-		for k := range evms {
-			availableIdentifiers = append(availableIdentifiers, k)
-		}
-		return fmt.Errorf("invalid EVM identifier, use one of: %v", availableIdentifiers)
+		return fmt.Errorf("invalid EVM identifier, use one of: %v", maps.Keys(evms))
 	}
 
 	filter, err := regexp.Compile(context.String("filter"))
@@ -100,127 +97,254 @@ func doRun(context *cli.Context) error {
 		jobCount = runtime.NumCPU()
 	}
 
+	seed := context.Uint64("seed")
+	maxErrors := context.Int("max-errors")
 	fullMode := context.Bool("full-mode")
 
-	var mutex sync.Mutex
-	var wg sync.WaitGroup
+	// Run the actual tests.
+	issues := runTests(evmIdentifier, evm, jobCount, seed, filter, fullMode, maxErrors)
 
-	var errorsPrinted atomic.Int32
-	var errorCount atomic.Int32
+	// Summarize the result.
+	if len(issues) == 0 {
+		fmt.Printf("All tests passed successfully!\n")
+		return nil
+	}
+
+	for _, issue := range issues {
+		// TODO: write input state of found issues into files
+		fmt.Printf("----------------------------\n")
+		fmt.Printf("%s\n", issue.err)
+	}
+	return fmt.Errorf("failed to pass %d test cases", len(issues))
+}
+
+// runTests orchestrates the parallel execution of all tests derived from the EVM
+// specification using numJobs parallel workers.
+func runTests(
+	evmName string,
+	evm ct.Evm,
+	numJobs int,
+	seed uint64,
+	filter *regexp.Regexp,
+	fullMode bool,
+	maxNumIssues int,
+) []issue {
+	// The execution of test cases is distributed to parallel goroutines in a three-step
+	// process:
+	//   - this goroutine writes the list of rules to be tested into a channel
+	//   - a team of goroutines fetches rules from the first channel, runs the
+	//     test state enumeration for the retrieved rule, and forward those states
+	//     into a second channel
+	//   - another team of goroutines fetches test-input states from the second
+	//     channel and processes the actual tests.
+	// Additionally, a goroutine periodically reporting progress information to the
+	// console is started.
+	// To avoid dead-locks in this goroutine, consuming goroutines are started before
+	// producing routines. Thus, the order in which goroutines and teams of goroutines
+	// are started below is in the reverse order as listed above.
+
+	var stateWaitGroup sync.WaitGroup
+	var testCounter atomic.Int64
 	var skippedCount atomic.Int32
+	issuesCollector := issuesCollector{}
 
-	ruleCh := make(chan rlz.Rule, jobCount)
+	fmt.Printf("Starting Conformance Tests on %v with seed %d ..\n", evmName, seed)
+	if maxNumIssues > 0 {
+		fmt.Printf("Testing will abort after identifying %d issue(s)\n", maxNumIssues)
+	} else {
+		maxNumIssues = math.MaxInt
+	}
 
-	spec := spc.Spec
-	for i := 0; i < jobCount; i++ {
-		wg.Add(1)
+	// Run a goroutine printing some progress information for the user.
+	done := make(chan bool)
+	printerDone := make(chan bool)
+	go func() {
+		defer close(printerDone)
+		ticker := time.NewTicker(5 * time.Second)
+		defer ticker.Stop()
+		startTime := time.Now()
+		lastTime := startTime
+		lastTestCounter := int64(0)
+		for {
+			select {
+			case <-done:
+				return
+			case curTime := <-ticker.C:
+				cur := testCounter.Load()
+
+				diffCounter := cur - lastTestCounter
+				diffTime := curTime.Sub(lastTime)
+
+				lastTime = curTime
+				lastTestCounter = cur
+
+				relativeTime := curTime.Sub(startTime)
+				rate := float64(diffCounter) / diffTime.Seconds()
+				fmt.Printf(
+					"[t=%4d:%02d] - Processing ~%s tests per second, total %d, skipped %d, found issues %d\n",
+					int(relativeTime.Seconds())/60, int(relativeTime.Seconds())%60,
+					unitconv.FormatPrefix(rate, unitconv.SI, 0), cur, skippedCount.Load(), issuesCollector.NumIssues(),
+				)
+			}
+		}
+	}()
+
+	// Run goroutines processing the actual tests.
+	stateWaitGroup.Add(numJobs)
+	stateChannel := make(chan *st.State, 10*numJobs)
+	for i := 0; i < numJobs; i++ {
 		go func() {
-			defer wg.Done()
-			for rule := range ruleCh {
-				tstart := time.Now()
+			defer stateWaitGroup.Done()
+			for input := range stateChannel {
+				if issuesCollector.NumIssues() >= maxNumIssues {
+					continue
+				}
+				testCounter.Add(1)
 
-				evaluationCount := 0
-				var errs []error
-				err := rule.EnumerateTestCases(rand.New(context.Uint64("seed")), func(state *st.State) rlz.ConsumerResult {
+				// TODO: program counter pointing to data not supported by LFVM
+				// converter. Fix this.
+				if evmName == "lfvm" && !input.Code.IsCode(int(input.Pc)) {
+					skippedCount.Add(1)
+					continue
+				}
+
+				if err := runTest(input, evm, filter); err != nil {
+					issuesCollector.AddIssue(input, err)
+				}
+			}
+		}()
+	}
+
+	// Generate test states in parallel (generation can be the bottleneck if there
+	// are many workers processing test cases in parallel).
+	ruleChannel := make(chan rlz.Rule, 10*numJobs)
+	var rulesWaitGroup sync.WaitGroup
+	rulesWaitGroup.Add(numJobs)
+	for i := 0; i < numJobs; i++ {
+		go func() {
+			defer rulesWaitGroup.Done()
+			for rule := range ruleChannel {
+				if issuesCollector.NumIssues() >= maxNumIssues {
+					continue
+				}
+				// random is re-seeded for each rule to be reproducible.
+				rand := rand.New(seed)
+				err := rule.EnumerateTestCases(rand, func(state *st.State) rlz.ConsumerResult {
+					if issuesCollector.NumIssues() >= maxNumIssues {
+						return rlz.ConsumeAbort
+					}
 					if !fullMode {
 						if applies, err := rule.Condition.Check(state); !applies || err != nil {
 							if err != nil {
-								errs = append(errs, err)
+								issuesCollector.AddIssue(state, err)
 							}
 							return rlz.ConsumeContinue
 						}
 					}
 
-					// TODO: program counter pointing to data not supported by LFVM
-					// converter.
-					if evmIdentifier == "lfvm" && !state.Code.IsCode(int(state.Pc)) {
-						skippedCount.Add(1)
-						return rlz.ConsumeContinue // ignored
-					}
-
-					evaluationCount++
-
-					input := state.Clone()
-
-					rules := spec.GetRulesFor(input)
-					if len(rules) == 0 {
-						// TODO: produce an error once spec is required to be complete
-						return rlz.ConsumeContinue // < ignore
-					}
-					expected := state.Clone()
-					rules[0].Effect.Apply(expected)
-
-					result, err := evm.StepN(input.Clone(), 1)
-					if err != nil {
-						errs = append(errs, err)
-						return rlz.ConsumeContinue
-					}
-
-					if !result.Eq(expected) {
-						errMsg := fmt.Sprintln(result.Diff(expected))
-						errMsg += fmt.Sprintln("input state:", input)
-						errMsg += fmt.Sprintln("result state:", result)
-						errMsg += fmt.Sprintln("expected state:", expected)
-						errs = append(errs, fmt.Errorf(errMsg))
-					}
-
+					stateChannel <- state.Clone()
 					return rlz.ConsumeContinue
 				})
 				if err != nil {
-					errs = append(errs, err)
+					issuesCollector.AddIssue(nil, fmt.Errorf("failed to enumerate test cases for %v: %w", rule, err))
 				}
-
-				// If no state was evaluated because it was skipped, this is not an error.
-				if evaluationCount == 0 && skippedCount.Load() == 0 {
-					errs = append(errs, fmt.Errorf("none of the generated states fulfilled all the conditions"))
-				}
-
-				ok := "OK"
-				if len(errs) > 0 {
-					ok = "Failed"
-				}
-
-				errorCount.Add(int32(len(errs)))
-
-				errorsToPrint := len(errs)
-				if maxErrors := context.Int("max-errors"); maxErrors > 0 {
-					errorsLeftToPrint := max(maxErrors-int(errorsPrinted.Load()), 0)
-					errorsToPrint = min(len(errs), errorsLeftToPrint)
-				}
-
-				errorsPrinted.Add(int32(errorsToPrint))
-
-				printErrors := errs[0:errorsToPrint]
-				err = errors.Join(printErrors...)
-
-				mutex.Lock()
-				{
-					fmt.Printf("%v: (rules evaluated: %v) %v (%v)\n", ok, evaluationCount, rule, time.Since(tstart).Round(10*time.Millisecond))
-
-					if err != nil {
-						fmt.Println(err)
-					}
-				}
-				mutex.Unlock()
 			}
 		}()
 	}
 
-	for _, rule := range spec.GetRules() {
+	// Feed the rule generator workers with rules.
+	rules := spc.Spec.GetRules()
+	rules = filterRules(rules, filter)
+	for _, rule := range rules {
+		ruleChannel <- rule
+	}
+	close(ruleChannel)
+	rulesWaitGroup.Wait()
+
+	close(stateChannel)
+	stateWaitGroup.Wait() // < releases when all test cases are processed
+
+	// Wait for the printer to be finished.
+	close(done)   // < signals progress printer to stop
+	<-printerDone // < blocks until channel is closed by progress printer
+
+	return issuesCollector.issues
+}
+
+// runTest runs a single test specified by the input state on the given EVM. The
+// function returns an error in case the execution did not work as expected.
+func runTest(input *st.State, evm ct.Evm, filter *regexp.Regexp) error {
+	rules := spc.Spec.GetRulesFor(input)
+	if len(rules) == 0 {
+		return nil // < TODO: make this an error once the specification is complete
+		//return fmt.Errorf("no rule found for state %v", input)
+	}
+
+	// filter out unwanted rules
+	rules = filterRules(rules, filter)
+	if len(rules) == 0 {
+		return nil // < this is fine, the targeted rules are filtered out by the user
+	}
+
+	// TODO: enable optional rule consistency check
+	rule := rules[0]
+	expected := input.Clone()
+	rule.Effect.Apply(expected)
+
+	result, err := evm.StepN(input.Clone(), 1)
+	if err != nil {
+		return fmt.Errorf("failed to process input state %v: %w", input, err)
+	}
+
+	if result.Eq(expected) {
+		return nil
+	}
+	errMsg := fmt.Sprintln("input state:", input)
+	errMsg += fmt.Sprintln("result state:", result)
+	errMsg += fmt.Sprintln("expected state:", expected)
+	errMsg += fmt.Sprintln("expectation defined by rule: ", rule.Name)
+	errMsg += "Differences:\n"
+	for _, diff := range result.Diff(expected) {
+		errMsg += fmt.Sprintf("\t%s\n", diff)
+	}
+	return fmt.Errorf(errMsg)
+}
+
+type issue struct {
+	input *st.State
+	err   error
+}
+
+type issuesCollector struct {
+	issues []issue
+	mu     sync.Mutex
+}
+
+func (c *issuesCollector) AddIssue(state *st.State, err error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	var clone *st.State
+	if state != nil {
+		clone = state.Clone()
+	}
+	c.issues = append(c.issues, issue{clone, err})
+}
+
+func (c *issuesCollector) NumIssues() int {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return len(c.issues)
+}
+
+func filterRules(rules []rlz.Rule, filter *regexp.Regexp) []rlz.Rule {
+	if filter == nil {
+		return rules
+	}
+	res := make([]rlz.Rule, 0, len(rules))
+	for _, rule := range rules {
 		if filter.MatchString(rule.Name) {
-			ruleCh <- rule
+			res = append(res, rule)
 		}
 	}
-
-	close(ruleCh)
-	wg.Wait()
-
-	if cnt := skippedCount.Load(); cnt > 0 {
-		fmt.Printf("Skipped tests: %d\n", cnt)
-	}
-	if cnt := errorCount.Load(); cnt > 0 {
-		return fmt.Errorf("total errors: %d", cnt)
-	}
-
-	return nil
+	return res
 }
