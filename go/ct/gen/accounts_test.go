282c02ec1c4e065616eb3c4c4bbbea18b0a70227
Facundo MH <facundo@ph3.at>
2024-04-03 17:34:22 +0200

    Add account and account generator benchmarks

--- go/ct/gen/accounts_test.go
+++ go/ct/gen/accounts_test.go
@@ -88,3 +88,17 @@ func TestAccountsGenerator_WarmColdConstraintsNoAssignment(t *testing.T) {
 		t.Errorf("Expected address to be cold but got warm")
 	}
 }
+
+func BenchmarkAccountGen(b *testing.B) {
+	v1 := Variable("v1")
+	v2 := Variable("v2")
+	assignment := Assignment{}
+	rnd := rand.New(0)
+	generator := NewAccountGenerator()
+
+	generator.BindWarm(v1)
+	generator.BindCold(v2)
+	for i := 0; i < b.N; i++ {
+		_, _ = generator.Generate(assignment, rnd, NewAddressFromInt(8))
+	}
+}
