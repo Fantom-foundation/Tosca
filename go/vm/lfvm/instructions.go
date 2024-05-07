c3b930802d8afbb5164c6a3d451c401202d5d27b
Facu <facumolina15@gmail.com>
2024-05-07 17:35:39 +0200

    LFVM: check for balance in callcode (#456)
    * Add check for balance in callcode and regression test for callcode

--- go/vm/lfvm/instructions.go
+++ go/vm/lfvm/instructions.go
@@ -1121,6 +1121,18 @@ func opCallCode(c *context) {
 		cost += vm.Gas(params.CallStipend)
 	}
 
+	// Check that the caller has enough balance to transfer the requested value.
+	if !value.IsZero() {
+		balance := c.context.GetBalance(c.params.Recipient)
+		balanceU256 := new(uint256.Int).SetBytes32(balance[:])
+		if balanceU256.Lt(value) {
+			c.stack.pushEmpty().Clear()
+			c.return_data = nil
+			c.gas += cost // the gas send to the nested contract is returned
+			return
+		}
+	}
+
 	// Get the arguments from the memory.
 	args := c.memory.GetSlice(inOffset.Uint64(), inSize.Uint64())
 	ret, err := c.context.Call(vm.CallCode, vm.CallParameter{
