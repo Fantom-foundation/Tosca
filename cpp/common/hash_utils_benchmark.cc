75709df478744ff620a9b748dfecc828c7a76c90
Herbert Jordan <herbert@swrnd.ch>
2024-06-17 12:19:10 +0200

    Unify license headers and make them mandatory

--- cpp/common/hash_utils_benchmark.cc
+++ cpp/common/hash_utils_benchmark.cc
@@ -1,4 +1,3 @@
-//
 // Copyright (c) 2024 Fantom Foundation
 //
 // Use of this software is governed by the Business Source License included
@@ -7,8 +6,7 @@
 // Change Date: 2028-4-16
 //
 // On the date above, in accordance with the Business Source License, use of
-// this software will be governed by the GNU Lesser General Public Licence v3.
-//
+// this software will be governed by the GNU Lesser General Public License v3.
 
 #include "common/hash_utils.h"
 
