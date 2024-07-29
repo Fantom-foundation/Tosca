2d0d457f630f1e8771631dff9d8a3ac82e0f7718
Luis Ayuso <luis@ph3.at>
2024-07-29 12:59:15 +0200

    remove /// comments in favour of //.

--- cpp/common/coverage/collect_coverage.c
+++ cpp/common/coverage/collect_coverage.c
@@ -3,8 +3,8 @@
 extern void __gcov_dump();
 #endif
 
-/// Reports whenever the library (and therefore the complete c++ project)
-/// was compiled with coverage flags.
+// Reports whenever the library (and therefore the complete c++ project)
+// was compiled with coverage flags.
 int IsCoverageEnabled() {
 #if defined(TOSCA_COVERAGE)
   return 1;
@@ -13,15 +13,15 @@ int IsCoverageEnabled() {
 #endif
 }
 
-/// When using Gcov, shared libraries do collect coverage data,
-/// but it is not automatically written into a file. Usually, GCC would
-/// add a corresponding call to the end of a `main` function, but it
-/// can not do this for a Go application. Thus, the dumping of
-/// coverage data of C++ library code needs to be triggered explicitly
-/// by calling this function at the end of an application.
-/// Calling this function will dump coverage data for all loaded C++ libraries
-/// compiled with coverage flags. If coverage data collection is disabled,
-/// this function is a no-op.
+// When using Gcov, shared libraries do collect coverage data,
+// but it is not automatically written into a file. Usually, GCC would
+// add a corresponding call to the end of a `main` function, but it
+// can not do this for a Go application. Thus, the dumping of
+// coverage data of C++ library code needs to be triggered explicitly
+// by calling this function at the end of an application.
+// Calling this function will dump coverage data for all loaded C++ libraries
+// compiled with coverage flags. If coverage data collection is disabled,
+// this function is a no-op.
 void DumpCoverageData() {
 #if defined(TOSCA_COVERAGE)
   // Since Gcc 11, before __gcov_flush()
