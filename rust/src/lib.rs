0a09940c668bee580542a45ae08378ee21b04d52
Lorenz Schüler <lorenz@saarow.de>
2024-07-29 16:25:59 +0200

    add evmc_create_steppable_evmrs to make linking work

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,18 +1,30 @@
-#[evmc_declare::evmc_declare_vm("This is an example VM name", "ewasm, evm", "1.2.3-custom")]
-pub struct ExampleVM;
+use evmc_vm::ffi::evmc_revision;
 
-impl evmc_vm::EvmcVm for ExampleVM {
+#[evmc_declare::evmc_declare_vm("evmrs", "ewasm, evm", "0.1.0")]
+pub struct EvmRs;
+
+impl evmc_vm::EvmcVm for EvmRs {
     fn init() -> Self {
-        ExampleVM {}
+        EvmRs {}
     }
 
     fn execute(
         &self,
-        revision: evmc_vm::ffi::evmc_revision,
+        revision: evmc_revision,
         code: &[u8],
         message: &evmc_vm::ExecutionMessage,
         context: Option<&mut evmc_vm::ExecutionContext>,
     ) -> evmc_vm::ExecutionResult {
         evmc_vm::ExecutionResult::success(1337, 0, None)
     }
+
+    fn set_option(&mut self, _: &str, _: &str) -> Result<(), evmc_vm::SetOptionError> {
+        Ok(())
+    }
+}
+
+#[no_mangle]
+extern "C" fn evmc_create_steppable_evmrs() -> *const ::evmc_vm::ffi::evmc_vm {
+    panic!("steppable not implemented");
+    unsafe { 0 as *const ::evmc_vm::ffi::evmc_vm }
 }
