602ea0d7b7fc631045b449a99a19dd00b2939aa9
Lorenz Schüler <lorenz@saarow.de>
2024-08-01 10:38:25 +0200

    add run function and call it from EvmcVm::execute and SteppableEvmcVm::step_n

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,7 +1,12 @@
+#![allow(dead_code)]
+use std::{i32, mem};
+
 use evmc_vm::{
     EvmcVm, ExecutionContext, ExecutionMessage, ExecutionResult, Revision, StatusCode, StepResult,
     StepStatusCode, SteppableEvmcVm, Uint256,
 };
+
+use crate::types::u256;
 
 mod ffi;
 mod types;
@@ -21,7 +26,23 @@ impl EvmcVm for EvmRs {
         message: &ExecutionMessage,
         context: Option<&mut ExecutionContext>,
     ) -> ExecutionResult {
-        ExecutionResult::success(1337, 0, None)
+        let step_result = run(
+            revision,
+            code,
+            message,
+            context,
+            StepStatusCode::EVMC_STEP_RUNNING,
+            0,
+            0,
+            Vec::with_capacity(1024),
+            Vec::new(),
+            None,
+            None,
+        );
+
+        step_result
+            .map(Into::into)
+            .unwrap_or_else(|(_, status_code)| ExecutionResult::new(status_code, 0, 0, None))
     }
 
     fn set_option(&mut self, _: &str, _: &str) -> Result<(), evmc_vm::SetOptionError> {
@@ -44,17 +65,62 @@ impl SteppableEvmcVm for EvmRs {
         last_call_result_data: &'a mut [u8],
         steps: i32,
     ) -> StepResult {
-        StepResult::new(
-            StepStatusCode::EVMC_STEP_FAILED,
-            StatusCode::EVMC_FAILURE,
+        run(
             revision,
-            0,
-            0,
-            0,
-            None,
-            Vec::new(),
-            Vec::new(),
-            None,
+            code,
+            message,
+            context,
+            step_status,
+            pc as usize,
+            gas_refund,
+            // SAFETY
+            // u256 is a newtype of Uint256 with repr(transparent) which guarantees the same memory
+            // layout.
+            unsafe { mem::transmute(stack.to_owned()) },
+            memory.to_owned(),
+            Some(last_call_result_data.to_owned()),
+            Some(steps),
         )
+        .unwrap_or_else(|(step_status_code, status_code)| {
+            StepResult::new(
+                step_status_code,
+                status_code,
+                revision,
+                0,
+                0,
+                0,
+                None,
+                Vec::new(),
+                Vec::new(),
+                None,
+            )
+        })
     }
+}
+
+fn run(
+    revision: Revision,
+    code: &[u8],
+    message: &ExecutionMessage,
+    context: Option<&mut ExecutionContext>,
+    mut step_status_code: StepStatusCode,
+    mut pc: usize,
+    mut gas_refund: i64,
+    mut stack: Vec<u256>,
+    mut memory: Vec<u8>,
+    mut last_call_return_data: Option<Vec<u8>>,
+    steps: Option<i32>,
+) -> Result<StepResult, (StepStatusCode, StatusCode)> {
+    Ok(StepResult::new(
+        StepStatusCode::EVMC_STEP_FAILED,
+        StatusCode::EVMC_FAILURE,
+        revision,
+        0,
+        0,
+        0,
+        None,
+        Vec::new(),
+        Vec::new(),
+        None,
+    ))
 }
