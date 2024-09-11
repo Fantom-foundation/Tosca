bb9e696b025d4c3333c428d80e7aa472ad6e5258
facuMH <facumolina15@gmail.com>
2024-09-11 16:07:33 +0200

    add set tests

--- go/interpreter/lfvm/instructions.go
+++ go/interpreter/lfvm/instructions.go
@@ -398,7 +398,7 @@ func opCallDataCopy(c *context) {
 		return
 	}
 
-	if err := c.memory.Set(memOffset64, length64, getData(c.params.Input, dataOffset64, length64)); err != nil {
+	if err := c.memory.set(memOffset64, length64, getData(c.params.Input, dataOffset64, length64)); err != nil {
 		c.signalError()
 	}
 }
@@ -832,7 +832,7 @@ func opCodeCopy(c *context) {
 		return
 	}
 	codeCopy := getData(c.params.Code, uint64CodeOffset, length.Uint64())
-	if err := c.memory.Set(memOffset.Uint64(), length.Uint64(), codeCopy); err != nil {
+	if err := c.memory.set(memOffset.Uint64(), length.Uint64(), codeCopy); err != nil {
 		c.signalError()
 	}
 }
@@ -1083,7 +1083,7 @@ func opExtCodeCopy(c *context) {
 		return
 	}
 	codeCopy := getData(c.context.GetCode(addr), uint64CodeOffset, length.Uint64())
-	if err = c.memory.Set(memOffset.Uint64(), length.Uint64(), codeCopy); err != nil {
+	if err = c.memory.set(memOffset.Uint64(), length.Uint64(), codeCopy); err != nil {
 		c.signalError()
 	}
 }
