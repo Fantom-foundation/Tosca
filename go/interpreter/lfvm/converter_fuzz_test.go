bf8fd19d5579c5796795e7404daca08440239d7f
Herbert Jordan <herbert@swrnd.ch>
2024-09-18 17:33:59 +0200

    Improve code conversion fuzzer

--- go/interpreter/lfvm/converter_fuzz_test.go
+++ go/interpreter/lfvm/converter_fuzz_test.go
@@ -11,10 +11,14 @@
 package lfvm
 
 import (
+	"math"
 	"testing"
 
 	"github.com/Fantom-foundation/Tosca/go/tosca/vm"
 )
+
+// To run this fuzzer use the following command:
+// go test ./interpreter/lfvm -run none -fuzz LfvmConverter --fuzztime 10m
 
 func FuzzLfvmConverter(f *testing.F) {
 
@@ -32,17 +36,14 @@ func FuzzLfvmConverter(f *testing.F) {
 		// also the limit for the conversion code (due to a 16-bit PC
 		// counter representation in the PC instruction). Thus, we test
 		// the code up to this level.
-		maxCodeSize := 1<<16 - 1
+		maxCodeSize := math.MaxUint16
 		if len(toscaCode) > maxCodeSize {
 			t.Skip()
 		}
 
-		type pair struct {
-			originalPos, lfvmPos int
-		}
-		var pairs []pair
+		mapping := make([]int, len(toscaCode))
 		lfvmCode := convertWithObserver(toscaCode, ConversionConfig{}, func(evm, lfvm int) {
-			pairs = append(pairs, pair{evm, lfvm})
+			mapping[evm] = lfvm
 		})
 
 		// Check that no super-instructions have been used.
@@ -53,28 +54,39 @@ func FuzzLfvmConverter(f *testing.F) {
 		}
 
 		// Check that all operations are mapped to matching operations.
-		for _, p := range pairs {
+		for i := 0; i < len(toscaCode); i++ {
+			originalPos := i
+			lfvmPos := mapping[originalPos]
 
-			toscaOpCode := vm.OpCode(toscaCode[p.originalPos])
-			lfvmOpCode := lfvmCode[p.lfvmPos].opcode
+			toscaOpCode := vm.OpCode(toscaCode[originalPos])
+			lfvmOpCode := lfvmCode[lfvmPos].opcode
 
-			if !vm.IsValid(toscaOpCode) && lfvmOpCode != INVALID {
-				t.Errorf("Expected INVALID, got %v", lfvmOpCode.String())
+			if !lfvmOpCode.isBaseInstruction() {
+				t.Errorf("Expected base instructions only, got %v", lfvmOpCode)
 			}
 
-			if vm.IsValid(toscaOpCode) {
-				if got, want := lfvmOpCode, OpCode(toscaOpCode); got != want {
-					t.Errorf("Expected %v, got %v", want, got)
+			if vm.OpCode(lfvmOpCode) != toscaOpCode {
+				t.Errorf("Invalid conversion from %v to %v", toscaOpCode, lfvmOpCode)
+			}
+
+			// Check that the position of JUMPDEST ops are preserved.
+			if toscaOpCode == vm.JUMPDEST {
+				if originalPos != lfvmPos {
+					t.Errorf("Expected JUMPDEST at %d, got %d", originalPos, lfvmPos)
 				}
 			}
-		}
 
-		// Check that the position of JUMPDEST ops are preserved.
-		for _, p := range pairs {
-			if vm.OpCode(toscaCode[p.originalPos]) == vm.JUMPDEST {
-				if p.originalPos != p.lfvmPos {
-					t.Errorf("Expected JUMPDEST at %d, got %d", p.originalPos, p.lfvmPos)
+			// Check that PC instructions point to the correct target.
+			if toscaOpCode == vm.PC {
+				target := int(lfvmCode[lfvmPos].arg)
+				if target != originalPos {
+					t.Errorf("Invalid PC target, wanted %d, got %d", originalPos, target)
 				}
+			}
+
+			// Skip the data section of PUSH instructions.
+			if vm.PUSH1 <= toscaOpCode && toscaOpCode <= vm.PUSH32 {
+				i += int(toscaOpCode-vm.PUSH1) + 1
 			}
 		}
 
