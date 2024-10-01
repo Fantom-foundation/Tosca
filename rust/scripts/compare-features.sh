add4541a94d6e08527e5a966632d2b838524e5ee
Lorenz Schüler <lorenz@saarow.de>
2024-10-01 13:11:07 +0200

    evmrs: add compare-features.sh

---
+++ rust/scripts/compare-features.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Run Rust benchmarks and create flamegraphs with different feature sets.
+# This script must be called from Tosca/rust with a list of features sets.
+
+# Requirements:
+# cargo install cargo-flamegraph
+
+if [ $(cat /proc/sys/kernel/perf_event_paranoid) -ne 0 ]; then
+    echo 'running: "echo 0 | sudo tee /proc/sys/kernel/perf_event_paranoid"'
+    echo 0 | sudo tee /proc/sys/kernel/perf_event_paranoid
+fi
+
+OUTPUT_DIR=profiling-results
+mkdir -p $OUTPUT_DIR
+
+INTERPRETER="evmrs"
+for FEATURES in "$@"; do
+    echo running with features: $FEATURES
+    cargo bench --package benchmarks --profile profiling --features "$FEATURES" | tee $OUTPUT_DIR/features#$FEATURES.criterion
+
+    FLAMEGRAPH_FILE=$(realpath ./$OUTPUT_DIR/features#$FEATURES.svg)
+    cargo flamegraph \
+        --freq 25000 \
+        --deterministic \
+        --flamechart \
+        --package benchmarks \
+        --profile profiling \
+        --features "$FEATURES" \
+        --output $FLAMEGRAPH_FILE
+
+    firefox $FLAMEGRAPH_FILE
+done
