a5ac94022a1ef3901039002ac9e1f13be774e876
Lorenz Schüler <lorenz@saarow.de>
2024-10-03 14:46:21 +0200

    evmrs: pass feature sets to bench.sh and document usage in BUILD.md

--- rust/scripts/bench.sh
+++ rust/scripts/bench.sh
@@ -1,48 +1,72 @@
 #!/bin/bash
 
-# Run Go interpreter benchmarks for evmzero, lfvm and geth in as well as for evmrs with different features in branch origin/evmrs-main.
-# This script must be called from the parent directory of Tosca.
+# Run Go interpreter benchmarks for evmzero, lfvm and geth as well as for evmrs with different feature sets on branch origin/evmrs-main.
+# This script must be called from the parent directory of Tosca with a list of feature sets.
+
+# Requirements:
+# go install golang.org/x/perf/cmd/benchstat@latest
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 [--evmrs-only] <feature-set> [<feature-set> ...]"
+    exit 1
+fi
+
+EVMRS_ONLY=0
+
+if [ "$1" == "--evmrs-only" ]; then
+    EVMRS_ONLY=1
+    shift
+
+    if [ $# -eq 0 ]; then
+        echo "Usage: $0 [--evmrs-only] <feature-set> [<feature-set> ...]"
+        exit 1
+    fi
+fi
 
 RUNS=20
 TIMEOUT=1h
 BRANCH="origin/evmrs-main"
 
-DATE=$(date +'%Y-%m-%dT%H:%M')
-
-BENCH_DIR=benches/$DATE
-mkdir -p $BENCH_DIR
-
 cd Tosca
 
 git fetch --all
-git checkout ${BRANCH}
-GIT_REF=$(git show-ref --hash=7 ${BRANCH})
+git checkout $BRANCH
 
-make
+DATE=$(date +'%Y-%m-%dT%H:%M')
+GIT_REF=$(git show-ref --hash=7 $BRANCH)
 
-# bench all non Rust interpreters
-INTERPRETERS=("evmzero" "lfvm" "geth")
-for INTERPRETER in "${INTERPRETERS[@]}"; do
-    BENCH_NAME=$(basename ${GIT_REF})-${INTERPRETER}-${RUNS}
-    echo running ${BENCH_NAME}
-    go test ./go/integration_test/interpreter \
-        --run none --bench ^Benchmark[a-zA-Z]+/./${INTERPRETER}$ \
-        --timeout $TIMEOUT --count $RUNS | tee ../${BENCH_DIR}/${BENCH_NAME}
-    sed -i "s/${INTERPRETER}-//g" ../${BENCH_DIR}/${BENCH_NAME}
-done
+OUTPUT_DIR=../benches/$DATE#$GIT_REF#$RUNS
+mkdir -p $OUTPUT_DIR
 
-# bench evmrs with different features
+if [ ! $EVMRS_ONLY ]; then
+    make
+
+    INTERPRETERS=("evmzero" "lfvm" "geth")
+    for INTERPRETER in "${INTERPRETERS[@]}"; do
+        echo running $INTERPRETER
+
+        OUTPUT_FILE=$OUTPUT_DIR/$INTERPRETER
+        go test ./go/integration_test/interpreter \
+            --run none --bench ^Benchmark[a-zA-Z]+/./$INTERPRETER$ \
+            --timeout $TIMEOUT --count $RUNS | tee $OUTPUT_FILE
+        sed -i "s/$INTERPRETER-//g" $OUTPUT_FILE
+    done
+fi
+
 INTERPRETER="evmrs"
-FEATURES_LIST=("" "performance")
-for FEATURES in "${FEATURES_LIST[@]}"; do
+for FEATURES in "$@"; do
+    echo running $INTERPRETER with features: $FEATURES
+
     cd rust
     cargo build --lib --release --features "$FEATURES"
     cd ..
 
-    BENCH_NAME=$(basename ${GIT_REF})-${INTERPRETER}-${FEATURES}-${RUNS}
-    echo running ${BENCH_NAME}
+    OUTPUT_FILE=$OUTPUT_DIR/$INTERPRETER-$FEATURES
     go test ./go/integration_test/interpreter \
-        --run none --bench ^Benchmark[a-zA-Z]+/./${INTERPRETER}$ \
-        --timeout $TIMEOUT --count $RUNS | tee ../${BENCH_DIR}/${BENCH_NAME}
-    sed -i "s/${INTERPRETER}-//g" ../${BENCH_DIR}/${BENCH_NAME}
+        --run none --bench ^Benchmark[a-zA-Z]+/./$INTERPRETER$ \
+        --timeout $TIMEOUT --count $RUNS | tee $OUTPUT_FILE
+    sed -i "s/$INTERPRETER-//g" $OUTPUT_FILE
 done
+
+cd $OUTPUT_DIR
+benchstat * | tee comparison
