b0da305ddfc9916de7d3f00f1d94ef6a972f53e3
Lorenz Schüler <lorenz@saarow.de>
2024-10-09 12:22:43 +0200

    evmrs: add feature "thread-local-cache"

--- rust/src/types/code_reader.rs
+++ rust/src/types/code_reader.rs
@@ -1,12 +1,13 @@
+#[cfg(feature = "jump-cache")]
+use std::num::NonZeroUsize;
+#[cfg(all(feature = "jump-cache", not(feature = "thread-local-cache")))]
+use std::sync::{Arc, LazyLock, Mutex};
+#[cfg(all(feature = "jump-cache", feature = "thread-local-cache"))]
+use std::{cell::RefCell, rc::Rc};
 use std::{
     cmp::min,
     mem::{self},
     ops::Deref,
-};
-#[cfg(feature = "jump-cache")]
-use std::{
-    num::NonZeroUsize,
-    sync::{Arc, LazyLock, Mutex},
 };
 
 use evmc_vm::StatusCode;
@@ -21,7 +22,7 @@ use crate::types::{code_byte_type, u256, CodeByteType, Opcode};
 const CACHE_SIZE: NonZeroUsize = unsafe { NonZeroUsize::new_unchecked(1 << 16) }; // taken from evmzero
 
 // Mutex<LruCache<...>> is faster that quick_cache::Cache<...>
-#[cfg(feature = "jump-cache")]
+#[cfg(all(feature = "jump-cache", not(feature = "thread-local-cache")))]
 static JUMP_CACHE: LazyLock<Mutex<LruCache<u256, Arc<[CodeByteType]>, BuildNoHashHasher<u64>>>> =
     LazyLock::new(|| {
         Mutex::new(LruCache::with_hasher(
@@ -29,6 +30,12 @@ static JUMP_CACHE: LazyLock<Mutex<LruCache<u256, Arc<[CodeByteType]>, BuildNoHas
             BuildNoHashHasher::default(),
         ))
     });
+
+#[cfg(all(feature = "jump-cache", feature = "thread-local-cache"))]
+thread_local! {
+    static JUMP_CACHE: RefCell<LruCache<u256, Rc<[CodeByteType]>, BuildNoHashHasher<u64>>> =
+        RefCell::new(LruCache::with_hasher(CACHE_SIZE, BuildNoHashHasher::default()));
+}
 
 #[derive(Debug, Clone, Copy)]
 pub struct PushLen(usize);
@@ -47,10 +54,12 @@ impl PushLen {
 #[derive(Debug)]
 pub struct CodeReader<'a> {
     code: &'a [u8],
-    #[cfg(feature = "jump-cache")]
-    code_byte_types: Arc<[CodeByteType]>,
     #[cfg(not(feature = "jump-cache"))]
     code_byte_types: Box<[CodeByteType]>,
+    #[cfg(all(feature = "jump-cache", not(feature = "thread-local-cache")))]
+    code_byte_types: Arc<[CodeByteType]>,
+    #[cfg(all(feature = "jump-cache", feature = "thread-local-cache"))]
+    code_byte_types: Rc<[CodeByteType]>,
     pc: usize,
 }
 
@@ -78,15 +87,37 @@ impl<'a> CodeReader<'a> {
                 // NOTE: The HashMap only stores the hash of the code, not the code itself (because
                 // this would require an allocation). This means a hash conflict
                 // will result in a wrong jump analysis.
-                code_byte_type = JUMP_CACHE
-                    .lock()
-                    .unwrap()
-                    .get_or_insert(code_hash.unwrap(), || {
-                        Arc::from(compute_code_byte_types(code).as_slice())
-                    })
-                    .clone();
+
+                #[cfg(all(feature = "jump-cache", not(feature = "thread-local-cache")))]
+                {
+                    code_byte_type = JUMP_CACHE
+                        .lock()
+                        .unwrap()
+                        .get_or_insert(code_hash.unwrap(), || {
+                            Arc::from(compute_code_byte_types(code).as_slice())
+                        })
+                        .clone();
+                }
+
+                #[cfg(all(feature = "jump-cache", feature = "thread-local-cache"))]
+                {
+                    code_byte_type = JUMP_CACHE.with_borrow_mut(|cache| {
+                        cache
+                            .get_or_insert(code_hash.unwrap(), || {
+                                Rc::from(compute_code_byte_types(code).as_slice())
+                            })
+                            .clone()
+                    });
+                }
             } else {
-                code_byte_type = Arc::from(compute_code_byte_types(code).as_slice());
+                #[cfg(all(feature = "jump-cache", not(feature = "thread-local-cache")))]
+                {
+                    code_byte_type = Arc::from(compute_code_byte_types(code).as_slice());
+                }
+                #[cfg(all(feature = "jump-cache", feature = "thread-local-cache"))]
+                {
+                    code_byte_type = Rc::from(compute_code_byte_types(code).as_slice());
+                }
             }
         }
         #[cfg(not(feature = "jump-cache"))]
--- rust/src/types/hash_cache.rs
+++ rust/src/types/hash_cache.rs
@@ -1,8 +1,9 @@
+#[cfg(all(feature = "jump-cache", feature = "thread-local-cache"))]
+use std::cell::RefCell;
 #[cfg(feature = "hash-cache")]
-use std::{
-    num::NonZeroUsize,
-    sync::{LazyLock, Mutex},
-};
+use std::num::NonZeroUsize;
+#[cfg(all(feature = "jump-cache", not(feature = "thread-local-cache")))]
+use std::sync::{LazyLock, Mutex};
 
 #[cfg(feature = "hash-cache")]
 use lru::LruCache;
@@ -14,12 +15,20 @@ use crate::u256;
 const CACHE_SIZE: NonZeroUsize = unsafe { NonZeroUsize::new_unchecked(1024) };
 
 // Mutex<LruCache<...>> is faster that quick_cache::Cache<...>
-#[cfg(feature = "hash-cache")]
+#[cfg(all(feature = "hash-cache", not(feature = "thread-local-cache")))]
 static HASH_CACHE_32: LazyLock<Mutex<LruCache<[u8; 32], u256>>> =
     LazyLock::new(|| Mutex::new(LruCache::new(CACHE_SIZE)));
-#[cfg(feature = "hash-cache")]
+#[cfg(all(feature = "hash-cache", not(feature = "thread-local-cache")))]
 static HASH_CACHE_64: LazyLock<Mutex<LruCache<[u8; 64], u256>>> =
     LazyLock::new(|| Mutex::new(LruCache::new(CACHE_SIZE)));
+
+#[cfg(all(feature = "hash-cache", feature = "thread-local-cache"))]
+thread_local! {
+static HASH_CACHE_32: RefCell<LruCache<[u8; 32], u256>> =
+    RefCell::new(LruCache::new(CACHE_SIZE));
+static HASH_CACHE_64: RefCell<LruCache<[u8; 64], u256>> =
+    RefCell::new(LruCache::new(CACHE_SIZE));
+}
 
 fn sha3(data: &[u8]) -> u256 {
     let mut hasher = Keccak256::new();
@@ -36,18 +45,28 @@ pub fn hash(data: &[u8]) -> u256 {
         // SAFETY:
         // data has length 32 so it is safe to cast it to &[u8; 32].
         let data = unsafe { &*(data.as_ptr() as *const [u8; 32]) };
-        *HASH_CACHE_32
+        #[cfg(all(feature = "hash-cache", not(feature = "thread-local-cache")))]
+        return *HASH_CACHE_32
             .lock()
             .unwrap()
-            .get_or_insert_ref(data, || sha3(data))
+            .get_or_insert_ref(data, || sha3(data));
+
+        #[cfg(all(feature = "hash-cache", feature = "thread-local-cache"))]
+        return HASH_CACHE_32
+            .with_borrow_mut(|cache| *cache.get_or_insert_ref(data, || sha3(data)));
     } else if data.len() == 64 {
         // SAFETY:
         // data has length 64 so it is safe to cast it to &[u8; 64].
         let data = unsafe { &*(data.as_ptr() as *const [u8; 64]) };
-        *HASH_CACHE_64
+        #[cfg(all(feature = "hash-cache", not(feature = "thread-local-cache")))]
+        return *HASH_CACHE_64
             .lock()
             .unwrap()
-            .get_or_insert_ref(data, || sha3(data))
+            .get_or_insert_ref(data, || sha3(data));
+
+        #[cfg(all(feature = "hash-cache", feature = "thread-local-cache"))]
+        return HASH_CACHE_64
+            .with_borrow_mut(|cache| *cache.get_or_insert_ref(data, || sha3(data)));
     } else {
         sha3(data)
     }
