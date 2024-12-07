501fef0d39247a90c63fe3fedcb767746240d24a
Lorenz Schüler <lorenz@saarow.de>
2024-12-07 18:44:01 +0100

    use unsafe extern blocks

--- rust/llvm-profile-wrappers/src/lib.rs
+++ rust/llvm-profile-wrappers/src/lib.rs
@@ -1,29 +1,22 @@
-extern "C" {
-    fn llvm_profile_enabled_wrapper() -> u8;
-    fn llvm_profile_set_filename_wrapper(filename: *const std::ffi::c_char);
-    fn llvm_profile_write_file_wrapper();
-    fn llvm_profile_reset_counters_wrapper();
+unsafe extern "C" {
+    safe fn llvm_profile_enabled_wrapper() -> u8;
+    safe fn llvm_profile_set_filename_wrapper(filename: *const std::ffi::c_char);
+    safe fn llvm_profile_write_file_wrapper();
+    safe fn llvm_profile_reset_counters_wrapper();
 }
 
 pub fn llvm_profile_enabled() -> u8 {
-    unsafe { llvm_profile_enabled_wrapper() }
+    llvm_profile_enabled_wrapper()
 }
 
-/// # Safety
-/// The provided filename can be a C string to set a new name or null to reset to the default
-/// behavior.
-pub unsafe fn llvm_profile_set_filename(filename: *const std::ffi::c_char) {
-    llvm_profile_set_filename_wrapper(filename);
+pub fn llvm_profile_set_filename(filename: Option<&std::ffi::c_char>) {
+    llvm_profile_set_filename_wrapper(filename.map(|f| &raw const *f).unwrap_or(std::ptr::null()));
 }
 
 pub fn llvm_profile_write_file() {
-    unsafe {
-        llvm_profile_write_file_wrapper();
-    }
+    llvm_profile_write_file_wrapper();
 }
 
 pub fn llvm_profile_reset_counters() {
-    unsafe {
-        llvm_profile_reset_counters_wrapper();
-    }
+    llvm_profile_reset_counters_wrapper();
 }
